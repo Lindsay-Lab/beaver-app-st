61b980e3d499513ac19fad59dc520e368eec98a6
Jack Etheredge <jack@twosense.ai>
2025-07-15 00:15:50 -0400

    deduplicate, centralize Earth Engine auth

--- pages/Quick_analysis.py
+++ pages/Quick_analysis.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import streamlit as st
-from google.oauth2 import service_account
 
 from service.Data_management import add_dam_buffer_and_standardize_date
+from service.earth_engine_auth import initialize_earth_engine
 from service.Negative_sample_functions import deduplicate_locations, prepareHydro, sampleNegativePoints
 from service.Parser import upload_points_to_ee
 from service.Visualize_trends import (
@@ -17,25 +17,7 @@ from service.Visualize_trends import (
     compute_all_metrics_up_downstream,
 )
 
-credentials_info = {
-    "type": st.secrets["gcp_service_account"]["type"],
-    "project_id": st.secrets["gcp_service_account"]["project_id"],
-    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-    "private_key": st.secrets["gcp_service_account"]["private_key"],
-    "client_email": st.secrets["gcp_service_account"]["client_email"],
-    "client_id": st.secrets["gcp_service_account"]["client_id"],
-    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
-    "universe_domain": st.secrets["gcp_service_account"]["universe_domain"],
-}
-
-credentials = service_account.Credentials.from_service_account_info(
-    credentials_info, scopes=["https://www.googleapis.com/auth/earthengine"]
-)
-
-ee.Initialize(credentials, project="ee-beaver-lab")
+initialize_earth_engine()
 
 st.title("Quick Analysis")
 
--- service/Visualize_trends.py
+++ service/Visualize_trends.py
@@ -1,27 +1,10 @@
 import ee
 import pandas as pd
 import streamlit as st
-from google.oauth2 import service_account
 
-credentials_info = {
-    "type": st.secrets["gcp_service_account"]["type"],
-    "project_id": st.secrets["gcp_service_account"]["project_id"],
-    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-    "private_key": st.secrets["gcp_service_account"]["private_key"],
-    "client_email": st.secrets["gcp_service_account"]["client_email"],
-    "client_id": st.secrets["gcp_service_account"]["client_id"],
-    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
-    "universe_domain": st.secrets["gcp_service_account"]["universe_domain"],
-}
+from .earth_engine_auth import initialize_earth_engine
 
-credentials = service_account.Credentials.from_service_account_info(
-    credentials_info, scopes=["https://www.googleapis.com/auth/earthengine"]
-)
-
-ee.Initialize(credentials)
+initialize_earth_engine()
 
 
 ####### Functions to for filtering collection
@@ -184,7 +167,7 @@ def S2_Export_for_visual_flowdir(Dam_Collection, filtered_waterway):
 
         # Change band names
         oldBandNames = ["B2", "B3", "B4", "B8", "cloudMask"]
-        newBandNames = ["S2_Blue", "S2_Green", "S2_Red", "S2_NIR", "S2_Binary_cloudMask"]  #'S2_NDVI']
+        newBandNames = ["S2_Blue", "S2_Green", "S2_Red", "S2_NIR", "S2_Binary_cloudMask"]  # 'S2_NDVI']
 
         S2_named_bands = S2_cloud_band.map(lambda image: image.select(oldBandNames).rename(newBandNames))
 
